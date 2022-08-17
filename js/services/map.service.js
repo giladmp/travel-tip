34c6429a904ccda34ad6038a38558bc5dd4886d2
Gilad Peled <Gilad>
2022-08-17 13:49:25 +0200

    first commit

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -39,7 +39,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyDUB5h052OFUAh1JfgvpFiMZ5k69QYaCvg' //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
